c109debb0699313921420126525c983bbd232866
mehismyname <46386283+mehismyname@users.noreply.github.com>
2021-02-22 17:56:53 -0500

    Update homework.py

--- homework/feb13/homework.py
+++ homework/feb13/homework.py
@@ -15,17 +15,24 @@ printing, calculations, and variables
 """
 
 # 1: In one sentence, please explain what a computer program is
-
+#It is a group of commands by a machine that can preform tasks
 # 2: What is the difference between SOFTWARE and HARDWARE?
-
+#Software are programs, and hardware are physical components
 # 3: Below, write a line of code that will print your name
-
+print('Kent Smith')
 # 4: Below, write some code that will print this:
+print('^  ^')
+print('____')
+
 """
 ^  ^
 ____
 """
 # 5: Below, write some code that will print this:
+print(' ___')
+print('|[_]|')
+print('|+ ;|')
+print('`---\'')
 """
  ___
 |[_]|
@@ -33,28 +40,28 @@ ____
 `---'
 """
 # 6: In python what type of symbol is '*'?
-
+#it is a star it can be a multiple symbol, or it is a import everything
 # 7: What is the output of the following code? (in other words, what the code will print)
 print(6 + 6 * 2 + 3)
-
+#it will print 21
 # 8: What is the output of the following code?
 print(44/(4 + 7) * 2)
-
+#It will print 8.0
 # 9: What is the output of the following code?
 print((2 + 2) * (2 + 2) / (2 + 2))
-
+#It will print 4.0
 # 10: What is the output of the following code?
 x = 3
 y = 120
 y = y + x
 print(y)
-
+#It will print 123
 # 11: What is the output of the following code?
 found_coins = 5
 magic_coins = 3
 stolen_coins = 5
 print((found_coins + stolen_coins) * stolen_coins)
-
+# it will print 50
 # 12: What is the output of the following code?
 x = 1
 y = 2
@@ -62,6 +69,6 @@ y = x + y
 john = y + x
 x = john
 print(john + x + y)
-
+#It will print 11
 # You are all done! Yay!
 # --- FINISHED ---
